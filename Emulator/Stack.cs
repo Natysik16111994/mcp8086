207ed4ebc260b3b63d466bdc3f37344d999f5e33
Natysik16111994 <Natysik16111994@mail.ru>
2016-03-29 23:51:57 +0900

    Stack

---
+++ Emulator/Stack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emulator
+{
+    class Stack
+    {
+        public List<BinaryNumber> list;
+
+        public Stack()
+        {
+            list = new List<BinaryNumber>();
+        }
+
+        public void Push(string n, int b)
+        {
+            bool[] c = BinaryNumber.GetBinary(BinaryNumber.GetDecimal(n, b));
+            for (int i = 0; i < 16; i++)
+                if (i < 8) 
+                
+        }
+    }
+}
